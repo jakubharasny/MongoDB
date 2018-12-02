ed877cb529b1833bd8ca354c2c84b161e1715010
Jakub <jakub.harasny@fuseuniversal.com>
2018-12-02 22:53:43 +0000

    add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 
+const { ObjectID } = require('mongodb')
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo')
 var {Users} = require('./models/users')
@@ -29,6 +30,18 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.get('/todos/:id', (req,res) => {
+  var id =req.params.id
+  if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.send(404).send()
+    }
+    res.status(200).send({todo})
+  }).catch((e) => { res.status(400).send() })
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000')
